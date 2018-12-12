5f2bdb9208152ec11f379d8be2b2c880d0aae6bf
leroipanda <30807838+leroipanda@users.noreply.github.com>
2018-12-12 17:55:22 +0100

    maj
    reste a faire la construction de l'arbre

--- livrable/src/decompression/src/decompression_juste.adb
+++ livrable/src/decompression/src/decompression_juste.adb
@@ -4,7 +4,8 @@ with Ada.Sequential_IO ;
 
 procedure Decompression_juste is
 
-   PACKAGE P_integer_file IS NEW Ada.Sequential_IO(integer) ;
+   type T_bit is mod 2;
+   PACKAGE P_integer_file IS NEW Ada.Sequential_IO(T_bit) ;
    USE P_integer_file ;
 
    Fichier : P_integer_file.File_type ;
@@ -13,16 +14,17 @@ procedure Decompression_juste is
    Taille_octet :constant Integer := 8;
    Taille_texte_encode :constant Integer :=4;
     Taille_arbre_encode :constant Integer :=1;
-    cara_test :constant character := 'ù';
+    cara_test :constant Character :='@' ;
 
    nom : String := "test1.txt";
    Type T_Tableau is array(1..Taille_table) of Character ;
    Type T_Table is array(1..Taille_octet) of T_Tableau;
+
     Table : T_Table;
 
 begin
    --ouverture et lecture du contenus du fichier( in : string nomfichier, out : File_type fichier)
-    P_integer_file.open(Fichier,In_File, "test.txt") ;
+    P_integer_file.open(Fichier,In_File, "jerome.txt.hf") ;
 
    --on itialise la table
    for i in 1..Taille_octet loop
@@ -38,46 +40,65 @@ begin
     declare
         function lire_octet(nombre :in integer) return integer is
             valeur :integer:=0;
-            valeur_lu : integer;
+            valeur_lu : T_bit;
         begin
 
-            for i in 1..nombre loop
-            read(Fichier,valeur_lu);
-                valeur := valeur*2 + valeur_lu;
+            for i in 1..nombre*8 loop
+                read(Fichier,valeur_lu);
+
+                if Integer(valeur_lu) = 1 then
+                    valeur := valeur*2 + 1;
+                else
+                    valeur := valeur*2 ;
+                end if;
+
             end loop;
+            put(integer'Image(valeur));
             return valeur;
         end lire_octet;
 
+         type cote is (Droite,Gauche);
         nb_caractere : Integer:=0;
         caractere_traduit : Integer:=0;
         taille_texte : integer;
         taille_arbre : integer;
         tableau_cara : T_Tableau;
         valeur : integer:=0;
-        valeur_lu :integer;
-        memoir :boolean := False;
+        valeur_lu :T_bit;
+        memoire :boolean := False;
         taille : integer :=0;
+        position_arbre : cote := Gauche;
+
 
 
     begin
         --lecture de la taille texte
         taille_texte := lire_octet(Taille_texte_encode);
+
         --lecture de taille arbre
         taille_arbre := lire_octet(Taille_arbre_encode);
         --lecture des caractere
+
         for i in 1..taille_arbre loop
             tableau_cara(i) := Character'Val(lire_octet(1));
         end loop;
         --
-
+        put_line("lecture parcour");
         --on lit le parcour de l'arbre
         while caractere_traduit /= taille_arbre loop
             read(Fichier,valeur_lu);
-            if valeur = 0 then
+            put_line("bit lu : " & integer'Image(integer(valeur_lu)) & " valeur : " & integer'Image(valeur));
+            if integer(valeur_lu) = 0 then
+                if memoire = false then
+                    position_arbre := droite;
+                else
+                    position_arbre := gauche;
+                end if ;
+
                 taille :=taille+1;
-                if memoir then
+                if position_arbre = Droite then
                     valeur := valeur*2 + 1;
-                    memoir := False;
+
                 else
                     valeur := valeur*2;
                 end if;
@@ -85,11 +106,17 @@ begin
             else
                 taille:= taille-1;
                 valeur := valeur / 2;
-                if not memoir then
-                    --on enregistre dans la la table
+                if memoire  then
+                    --on remonte
+                    null;
+                else
+                     --on remonte et
+                     --on enregistre
                     caractere_traduit := caractere_traduit+1;
-                    table(taille)(valeur) := tableau_cara(caractere_traduit);
-                    memoir := true;
+                    table(taille)(valeur+1) := tableau_cara(caractere_traduit);
+                    put_line( character'Image(tableau_cara(caractere_traduit)) & " = " & Integer'Image(valeur) );
+                    memoire := true;
+
 
                 end if;
 
@@ -106,7 +133,7 @@ begin
       valeurHuffman : Integer;
       taille : Integer:=1;
       convertie : Boolean;
-      inte_lu :  integer ;
+      inte_lu :  T_bit ;
 
 
       begin
@@ -119,8 +146,12 @@ begin
          taille := 1;
          valeurHuffman :=0;
          while not convertie loop
-         read(fichier, inte_lu);
-         valeurHuffman := valeurHuffman *2   +inte_lu ;
+                read(fichier, inte_lu);
+                  if Integer(inte_lu )= 1 then
+                    valeurHuffman := valeurHuffman *2   +1 ;
+                else
+                    valeurHuffman := valeurHuffman *2   ;
+                end if;
 
             if Table(taille)(valeurHuffman+1) /=  cara_test  then
               -- Put_Line("---entree---");
